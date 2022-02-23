1de3f5680f97ced5f385ee9532b72832763d679d
Jiří Špác <capajj@gmail.com>
2022-02-23 12:08:14 +0100

    remove dist from git again

--- dist/index.js
+++
@@ -1,8 +0,0 @@
-
-'use strict'
-
-if (process.env.NODE_ENV === 'production') {
-  module.exports = require('./zod-prisma.cjs.production.min.js')
-} else {
-  module.exports = require('./zod-prisma.cjs.development.js')
-}
