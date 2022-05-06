f4d99eec0b7a5662c796cf711425c1a780faf66f
omar-dulaimi <o.m.dulaimi@gmail.com>
2022-05-06 19:50:53 +0300

    handle missing zod dependency

--- bin/cli.js
+++ bin/cli.js
@@ -1,3 +1,14 @@
 #!/usr/bin/env node
 
+try {
+	require('zod')
+} catch (error) {
+	if (error.code !== 'MODULE_NOT_FOUND') {
+		throw error
+	}
+	var RED = '\x1b[31m%s\x1b[0m'
+	console.error(RED, 'Please makes sure to install zod first.')
+	process.exit(1)
+}
+
 require('../dist/index')
