50b523ade51d32a203013c0df96d73814cfcad94
Carter Grimmeisen <cpg0009@uah.edu>
2022-06-30 18:26:17 +0000

    Fixed check for missing zod dep

--- src/cli.ts
+++ src/cli.ts
@@ -1,3 +1,15 @@
 #!/usr/bin/env node
 
-import "./index"
+async function main() {
+  try {
+    await import("zod")
+  } catch (e) {
+    const RED = "\x1b[31m%s\x1b[0m"
+    console.error(RED, "Please makes sure to install zod first.")
+    process.exit(1)
+  } finally {
+    import("./index")
+  }
+}
+
+main()
